6717bef6f766fe02ff01997c02ce981885952656
Kevin Savage <broote@gmail.com>
2022-09-19 15:42:20 -0400

    First Commit.

---
+++ README.md
@@ -0,0 +1,32 @@
+# Basic Save Game Batch Script
+This is a batch script I use to backup save game files on my PC.
+
+The file checked in is the version I use to backup satisfactory.  Copy and rename as needed.
+
+## How to use
+Simply change the SAVES_LOC to where your save game files are located, and the BACKUP_LOC to where you want the files to be copied.
+
+File locations are expected to be a full path inside double quotes with no trailing slash.
+
+### Examples for some game's I've used this for:
+
+**Satisfactory**
+```
+set SAVES_LOC="%LocalAppData%\FactoryGame\Saved\SaveGames"
+
+set BACKUP_LOC="F:\Backups\Satisfactory"
+```
+
+**No Man's Sky**
+```
+set SAVES_LOC="%APPDATA%\HelloGames\NMS"
+
+set BACKUP_LOC="F:\Backups\NMS"
+```
+
+**Cyberpunk 2077**
+```
+set SAVES_LOC="%USERPROFILE%\Saved Games\CD Projekt Red\Cyberpunk 2077"
+
+set BACKUP_LOC="F:\Backups\Cyberpunk2077"
+```
---
+++ Satisfactory-Backup.bat
@@ -0,0 +1,33 @@
+echo off
+
+REM Backup save file Location: (Full path inside double quotes, no trailing slash)
+set SAVES_LOC="%LocalAppData%\FactoryGame\Saved\SaveGames"
+
+REM Destination for Backup files: (Full path inside double quotes, no trailing slash)
+set BACKUP_LOC="F:\Backups\Satisfactory"
+
+REM ---- No Edits Needed Below this Line ----
+
+REM Make a new folder name using date and time.
+set CUR_YYYY=%date:~10,4%
+set CUR_MM=%date:~4,2%
+set CUR_DD=%date:~7,2%
+set CUR_HH=%time:~0,2%
+if %CUR_HH% lss 10 (set CUR_HH=0%time:~1,1%)
+
+set CUR_NN=%time:~3,2%
+set CUR_SS=%time:~6,2%
+set CUR_MS=%time:~9,2%
+
+REM New Folder name: YYYY-MM-DD_HHNNSS
+set DIR_NAME=%CUR_YYYY%-%CUR_MM%-%CUR_DD%_%CUR_HH%%CUR_NN%%CUR_SS%
+
+REM Make the destination folder
+mkdir %BACKUP_LOC%%DIR_NAME%
+
+REM Copy all the things! _o/
+robocopy %SAVES_LOC% %BACKUP_LOC%%DIR_NAME% /E
+
+echo Done.
+REM Wait for user to exit, I hate it when console windows close so fast I don't know what it was.
+pause
